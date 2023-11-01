1d0c8d356ff08d54cae1a0ca5c6e971c1a99ce00
mir_faisal <mirfaisal08@gmail.comm>
2023-11-02 01:21:53 +0600

    created productss contexAPI for global access

---
+++ src/data/data.js
@@ -0,0 +1,48 @@
+const products = [
+    {
+        id: 1,
+        img: './product_images/image-1.webp'
+    },
+    {
+        id: 2,
+        img: './product_images/image-2.webp'
+    },
+    {
+        id: 3,
+        img: './product_images/image-3.webp'
+    },
+    {
+        id: 4,
+        img: './product_images/image-4.webp'
+    },
+    {
+        id: 5,
+        img: './product_images/image-5.webp'
+    },
+    {
+        id: 6,
+        img: './product_images/image-6.webp'
+    },
+    {
+        id: 7,
+        img: './product_images/image-7.webp'
+    },
+    {
+        id: 8,
+        img: './product_images/image-8.webp'
+    },
+    {
+        id: 9,
+        img: './product_images/image-9.webp'
+    },
+    {
+        id: 10,
+        img: './product_images/image-10.jpge'
+    },
+    {
+        id: 11,
+        img: './product_images/image-11.jpge'
+    },
+    
+    
+]
